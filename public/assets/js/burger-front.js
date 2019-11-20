8ab3a2f7823d2d49bdb2b4902c2ff0fa6c217670
Dalton Maurer <dmaurer3394@gmail.com>
2019-11-20 17:04:31 -0500

    styling and layout changed

--- public/assets/js/burger-front.js
+++ public/assets/js/burger-front.js
@@ -1,4 +1,3 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
   $(".change-eaten").on("click", function(event) {
     var id = $(this).data("id");
@@ -8,37 +7,29 @@ $(function() {
       devoured: newEat
     };
 
-    // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newEatState
     }).then(function() {
       console.log("changed devoured to", newEat);
-      // Reload the page to get the updated list
       location.reload();
     });
   });
 
   $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
     var newBurger = {
       burger_name: $("#burger")
         .val()
-        .trim(),
-      devoured: $("[name=eaten]:checked")
-        .val()
         .trim()
     };
 
-    // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
     }).then(function() {
       console.log("created new burger");
-      // Reload the page to get the updated list
       location.reload();
     });
   });
@@ -46,12 +37,10 @@ $(function() {
   $(".delete-burger").on("click", function(event) {
     var id = $(this).data("id");
 
-    // Send the DELETE request.
     $.ajax("/api/burgers/" + id, {
       type: "DELETE"
     }).then(function() {
       console.log("deleted burger", id);
-      // Reload the page to get the updated list
       location.reload();
     });
   });
